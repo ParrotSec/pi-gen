8ad2e0693f9a72eeab2f3464a6705b4397e9d7f9
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2018-11-10 20:35:53 +0100

    update bootstrap parameters

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 if [ ! -d "${ROOTFS_DIR}" ]; then
-	bootstrap stretch "${ROOTFS_DIR}" http://deb.parrotsec.org/parrot/
+	bootstrap stable "${ROOTFS_DIR}" http://deb.parrotsec.org/parrot/
 fi
